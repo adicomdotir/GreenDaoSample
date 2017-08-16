2967fdbfbb7a20a0a876d25b6ab28b6ddc7fc744
Adicom <adicom.ir@gmail.com>
2017-08-16 19:21:11 +0430

    Fix issue

--- app/src/main/java/ir/adicom/app/greendaoapplication/AddEditDailogFragment.java
+++ app/src/main/java/ir/adicom/app/greendaoapplication/AddEditDailogFragment.java
@@ -15,6 +15,8 @@ import android.widget.Toast;
 
 import ir.adicom.app.greendaoapplication.Models.Event;
 import ir.adicom.app.greendaoapplication.Models.EventDao;
+import ir.adicom.app.greendaoapplication.Models.Province;
+import ir.adicom.app.greendaoapplication.Models.ProvinceDao;
 
 
 /**
@@ -43,37 +45,63 @@ public class AddEditDailogFragment extends DialogFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        final EventDao eventDao = ((DemoApp)getActivity().getApplication()).getDaoSession().getEventDao();
         Button dismiss = (Button) view.findViewById(R.id.btn_cancel);
+        Button btnAdd = (Button) view.findViewById(R.id.btn_add);
+        final EditText etTitle = (EditText) view.findViewById(R.id.et_title);
         dismiss.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dismiss();
             }
         });
-        final EditText etTitle = (EditText) view.findViewById(R.id.et_title);
-        Button btnAdd = (Button) view.findViewById(R.id.btn_add);
-        Event event = null;
-        if(id > 0) {
-            event = eventDao.load(id);
-            etTitle.setText(event.getName());
-            btnAdd.setText(R.string.edit_text);
-        }
-        final EventActivity eventActivity = (EventActivity) getActivity();
-        final Event finalEvent = event;
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(id > 0) {
-                    finalEvent.setName(etTitle.getText().toString());
-                    eventDao.update(finalEvent);
-                } else {
-                    eventDao.insert(new Event(null, etTitle.getText().toString()));
-                }
-                eventActivity.onResume();
-                dismiss();
+        if(getActivity() instanceof EventActivity) {
+            final EventDao eventDao = ((DemoApp) getActivity().getApplication()).getDaoSession().getEventDao();
+            Event event = null;
+            if (id > 0) {
+                event = eventDao.load(id);
+                etTitle.setText(event.getName());
+                btnAdd.setText(R.string.edit_text);
             }
-        });
+            final EventActivity eventActivity = (EventActivity) getActivity();
+            final Event finalEvent = event;
+            btnAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (id > 0) {
+                        finalEvent.setName(etTitle.getText().toString());
+                        eventDao.update(finalEvent);
+                    } else {
+                        eventDao.insert(new Event(null, etTitle.getText().toString()));
+                    }
+                    eventActivity.onResume();
+                    dismiss();
+                }
+            });
+        }
+        if(getActivity() instanceof ProvinceActivity) {
+            final ProvinceDao provinceDao = ((DemoApp) getActivity().getApplication()).getDaoSession().getProvinceDao();
+            Province province = null;
+            if (id > 0) {
+                province = provinceDao.load(id);
+                etTitle.setText(province.getTitle());
+                btnAdd.setText(R.string.edit_text);
+            }
+            final ProvinceActivity eventActivity = (ProvinceActivity) getActivity();
+            final Province finalProvince = province;
+            btnAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (id > 0) {
+                        finalProvince.setTitle(etTitle.getText().toString());
+                        provinceDao.update(finalProvince);
+                    } else {
+                        provinceDao.insert(new Province(null, etTitle.getText().toString()));
+                    }
+                    eventActivity.onResume();
+                    dismiss();
+                }
+            });
+        }
     }
 
 
